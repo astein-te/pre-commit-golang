95098d2f51b34faf9683be2e0f11ef970e0c5061
Alexander Stein <astein@thousandeyes.com>
2021-03-19 14:04:08 -0500

    rework run-staticcheck.sh to just use xargs on multiline input

--- run-staticcheck.sh
+++ run-staticcheck.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-PACKAGES=()
-for PACKAGE in $(go list ./... | grep -v /vendor/); do
-  PACKAGES+=($PACKAGE)
-done
-exec staticcheck $PACKAGES
+go list ./... | grep -v /vendor/ | xargs staticcheck
